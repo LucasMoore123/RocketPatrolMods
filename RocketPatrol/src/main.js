9e6c6ffe19c461d89a6dc43fc4719231a58c388d
LucasMoore123 <66981809+LucasMoore123@users.noreply.github.com>
2022-04-12 08:56:35 -0700

    Add files via upload

--- RocketPatrol/src/main.js
+++ RocketPatrol/src/main.js
@@ -1,15 +1,14 @@
 let config = {
-    type: Phaser.CANVAS,
-    width: 640,
-    height: 480,
-    scene: [ Menu, Play ]
-}
+   type: Phaser.AUTO,
+   width: 640,
+   height : 480,
+   scene: [ Menu, Play ]
+};
+
+let borderUISize = config.height / 15;
+let borderPadding = borderUISize / 3;
 
 let game = new Phaser.Game(config);
 
-// set UI sizes
-let borderUISize = game.config.height / 15;
-let borderPadding = borderUISize / 3;
-
-// reserve keyboard variables
+// reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
--- RocketPatrol/src/prefabs/Spaceship.js
+++ RocketPatrol/src/prefabs/Spaceship.js
@@ -1,23 +1,24 @@
 // Spaceship prefab
 class Spaceship extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame, pointValue) {
-        super(scene, x, y, texture, frame);
-        scene.add.existing(this);   // add to existing scene
-        this.points = pointValue;   // store pointValue
-        this.moveSpeed = game.settings.spaceshipSpeed;         // pixels per frame
-    }
+   constructor(scene, x, y, texture, frame, pointValue) {
+     super(scene, x, y, texture, frame);
+     // add object to existing scene
+     scene.add.existing(this);
+     this.points = pointValue;
+     this.moveSpeed = game.settings.spaceshipSpeed;
+   }
 
-    update() {
-        // move spaceship left
-        this.x -= this.moveSpeed;
-        // wrap around from left edge to right edge
-        if(this.x <= 0 - this.width) {
-            this.reset();
-        }
-    }
+   update() {
+      // move spaceship left
+      this.x -= this.moveSpeed;
+      // wrap around from left edge to right edge
+      if (this.x <= 0 - this.width) {
+         this.x = game.config.width;
+      }
+   }
 
-    // position reset
-    reset() {
-        this.x = game.config.width;
-    }
+   // position reset
+   reset() {
+      this.x = game.config.width;
+   }
 }
